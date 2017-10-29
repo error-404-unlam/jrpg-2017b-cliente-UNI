5eac9316bc21b0a390a7a5d8a1bfa0f7e2cf590d
Miguel Angel Amengual <amengualma6@gmail.com>
2017-10-29 18:18:08 -0300

    Se eliminan los errores del CheckStyle de la clase ActualizarComercio

--- src/main/java/comandos/ActualizarInventario.java
+++ src/main/java/comandos/ActualizarInventario.java
@@ -2,18 +2,24 @@ package comandos;
 
 import mensajeria.PaquetePersonaje;
 
+/**
+ * Comando Actualizar Inventario.
+ * @author Miguel
+ */
 public class ActualizarInventario extends ComandosEscucha {
 
 	@Override
 	public void ejecutar() {
-		PaquetePersonaje paquetePersonaje = (PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class);
+		PaquetePersonaje paquetePersonaje = (PaquetePersonaje) gson.
+				fromJson(cadenaLeida, PaquetePersonaje.class);
 		juego.getPersonajesConectados().remove(paquetePersonaje.getId());
 		juego.getPersonajesConectados().put(paquetePersonaje.getId(), paquetePersonaje);
 		if (juego.getPersonaje().getId() == paquetePersonaje.getId()) {
 			juego.actualizarPersonaje();
 			juego.getEstadoJuego().actualizarPersonaje();
 			juego.getCli().actualizarItems(paquetePersonaje);
-			juego.getCli().actualizarPersonaje(juego.getPersonajesConectados().get(paquetePersonaje.getId()));
+			juego.getCli().actualizarPersonaje(juego.getPersonajesConectados().
+					get(paquetePersonaje.getId()));
 		}
 
 	}
