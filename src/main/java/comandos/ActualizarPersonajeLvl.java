57bf8662bf683d8dd9672c8b59cc277dc3ed8801
Miguel Angel Amengual <amengualma6@gmail.com>
2017-10-29 18:37:58 -0300

    Se eliminan los errores del CheckStyle de la clase ActualizarPersonajeLv

--- src/main/java/comandos/ActualizarPersonajeLvl.java
+++ src/main/java/comandos/ActualizarPersonajeLvl.java
@@ -2,11 +2,16 @@ package comandos;
 
 import mensajeria.PaquetePersonaje;
 
+/**
+ * Comando Actualizar Nivel Del Personaje
+ * @author Miguel
+ */
 public class ActualizarPersonajeLvl extends ComandosEscucha {
 
 	@Override
 	public void ejecutar() {
-		PaquetePersonaje paquetePersonaje = (PaquetePersonaje) gson.fromJson(cadenaLeida, PaquetePersonaje.class);
+		PaquetePersonaje paquetePersonaje = (PaquetePersonaje) gson.
+				fromJson(cadenaLeida, PaquetePersonaje.class);
 
 		juego.getPersonajesConectados().remove(paquetePersonaje.getId());
 		juego.getPersonajesConectados().put(paquetePersonaje.getId(), paquetePersonaje);
@@ -14,7 +19,8 @@ public class ActualizarPersonajeLvl extends ComandosEscucha {
 		if (juego.getPersonaje().getId() == paquetePersonaje.getId()) {
 			juego.actualizarPersonaje();
 			juego.getEstadoJuego().actualizarPersonaje();
-			juego.getCli().actualizarPersonaje(juego.getPersonajesConectados().get(paquetePersonaje.getId()));
+			juego.getCli().actualizarPersonaje(juego.getPersonajesConectados().
+					get(paquetePersonaje.getId()));
 		}
 
 	}
