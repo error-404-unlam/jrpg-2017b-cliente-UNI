8617f42606565944784f8b812535db7c70ac621e
Leonel San Martin <31077603+leo9210@users.noreply.github.com>
2017-10-30 13:53:39 -0300

    Correcion de incidencias

--- src/main/java/inventario/Celda.java
+++ src/main/java/inventario/Celda.java
@@ -17,26 +17,53 @@ import dominio.Item;
 import mensajeria.PaquetePersonaje;
 import recursos.Recursos;
 
+/**
+ * Clase Celda
+ *
+ * @author lesanmartin
+ *
+ */
 public class Celda extends JPanel {
 
 	private BufferedImage item;
 	private PaquetePersonaje paquetePersonaje;
 	private JLabel label;
 	private Item it;
+	private static final int ANCHO_IMAGEN = 49;
+	private static final int ALTURA_IMAGEN = 49;
+	private static final int ANCHO_DIMENSION = 60;
+	private static final int ALTURA_DIMENSION = 49;
 
-	public Celda(Item item, PaquetePersonaje paquetePersonaje) throws IOException {
+	/**
+	 * Constructor parametrizado de la clase
+	 *
+	 * @param item
+	 * @param paquetePersonaje
+	 * @throws IOException
+	 */
+	public Celda(final Item item, final PaquetePersonaje paquetePersonaje) throws IOException {
 		this.item = item.getFoto();
 		it = item;
 		this.paquetePersonaje = paquetePersonaje;
-		label = new JLabel(new ImageIcon(this.item.getScaledInstance(49, 49, Image.SCALE_DEFAULT)));
+		label = new JLabel(
+				new ImageIcon(this.item.getScaledInstance(ANCHO_IMAGEN, ALTURA_IMAGEN, Image.SCALE_DEFAULT)));
 		actionListenersYLabel(item);
 	}
 
+	/**
+	 * Constructor por defecto de la clase
+	 */
 	public Celda() {
-		label = new JLabel(new ImageIcon(Recursos.noItem.getScaledInstance(49, 49, Image.SCALE_DEFAULT)));
+		label = new JLabel(
+				new ImageIcon(Recursos.noItem.getScaledInstance(ANCHO_IMAGEN, ALTURA_IMAGEN, Image.SCALE_DEFAULT)));
 		add(label);
 	}
 
+	/**
+	 * Acciones de item
+	 *
+	 * @param item
+	 */
 	private void actionListenersYLabel(Item item) {
 		StringBuilder s = new StringBuilder();
 
@@ -70,31 +97,48 @@ public class Celda extends JPanel {
 
 	}
 
+	/**
+	 * Reseteo de label
+	 */
 	protected void resetLabel() {
-		label.setIcon(new ImageIcon(Recursos.noItem.getScaledInstance(49, 49, Image.SCALE_DEFAULT)));
+		label.setIcon(
+				new ImageIcon(Recursos.noItem.getScaledInstance(ANCHO_IMAGEN, ALTURA_IMAGEN, Image.SCALE_DEFAULT)));
 		label.setToolTipText(null);
 		paquetePersonaje.removerItem(it);
 		label.removeMouseListener(mouseListener);
 		removeMouseListener(mouseListener);
 	}
 
+	/**
+	 * Retorna el tamaño de la dimension
+	 */
 	@Override
 	public Dimension getPreferredSize() {
-		return new Dimension(60, 60);
+		return new Dimension(ANCHO_DIMENSION, ALTURA_DIMENSION);
 	}
 
+	/**
+	 * Retorna el label
+	 *
+	 * @return
+	 */
 	public JLabel getLabel() {
 		return label;
 	}
 
-	MouseListener mouseListener = new MouseAdapter() {
+	/**
+	 * Listener del mouse
+	 */
+	private MouseListener mouseListener = new MouseAdapter() {
 		public void mouseClicked(MouseEvent e) {
-			Object[] options = { "Tirar", "Cancelar" };
+			Object[] options = {"Tirar", "Cancelar"};
 			if (e.getClickCount() == 2) {
-				int answer = JOptionPane.showOptionDialog(getParent(), "¿Qué desea hacer?", "Item: " + it.getNombre(), JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[1]);
+				int answer = JOptionPane.showOptionDialog(getParent(), "¿Qué desea hacer?", "Item: " + it.getNombre(),
+						JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[1]);
 				// Tirar
 				if (answer == 0) {
-					paquetePersonaje.sacarBonus(it.getBonusSalud(), it.getBonusEnergia(), it.getBonusFuerza(), it.getBonusDestreza(), it.getBonusInteligencia());
+					paquetePersonaje.sacarBonus(it.getBonusSalud(), it.getBonusEnergia(), it.getBonusFuerza(),
+							it.getBonusDestreza(), it.getBonusInteligencia());
 					resetLabel();
 				}
 			}
