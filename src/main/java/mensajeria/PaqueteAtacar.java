b474110eaf52e85ea75880fca7dc70fb2b8c25f4
Miguel Angel Amengual <amengualma6@gmail.com>
2017-11-01 17:13:31 -0300

    Se eliminan los errores  CheckStyle PaqueteAtacar

--- src/main/java/mensajeria/PaqueteAtacar.java
+++ src/main/java/mensajeria/PaqueteAtacar.java
@@ -3,6 +3,10 @@ package mensajeria;
 import java.io.Serializable;
 import java.util.HashMap;
 
+/**
+ * Paquete a ser enviado cuando un jugador ataca
+ * @author Miguel
+ */
 public class PaqueteAtacar extends Paquete implements Serializable, Cloneable {
 
 	private int id;
@@ -14,7 +18,25 @@ public class PaqueteAtacar extends Paquete implements Serializable, Cloneable {
 	private HashMap<String, Number> mapPersonaje = new HashMap<String, Number>();
 	private HashMap<String, Number> mapEnemigo = new HashMap<String, Number>();
 
-	public PaqueteAtacar(int id, int idEnemigo, int nuevaSalud, int nuevaEnergia, int nuevaSaludEnemigo, int nuevaEnergiaEnemigo, int nuevaDefensa, int nuevaDefensaEnemigo, double probEvitarDano, double probEvitarDanoEnemgio) {
+	/**
+	 * Constructor
+	 * @param id id personaje personaje
+	 * @param idEnemigo id del enemigo
+	 * @param nuevaSalud nueva salud personaje
+	 * @param nuevaEnergia nueva energia personaje
+	 * @param nuevaSaludEnemigo nueva salud del enemigo
+	 * @param nuevaEnergiaEnemigo nueva energia del enemigo
+	 * @param nuevaDefensa nueva defensa personaje
+	 * @param nuevaDefensaEnemigo nueva defensa enemigo
+	 * @param probEvitarDano probabilidad de evitar danio
+	 * @param probEvitarDanoEnemgio probabilidad de evitar daño del enemigo
+	 */
+	public PaqueteAtacar(
+			final int id, final int idEnemigo,
+			final int nuevaSalud, final int nuevaEnergia,
+			final int nuevaSaludEnemigo, final  int nuevaEnergiaEnemigo,
+			final int nuevaDefensa, final int nuevaDefensaEnemigo,
+			final double probEvitarDano, final double probEvitarDanoEnemgio) {
 		setComando(Comando.ATACAR);
 		this.id = id;
 		this.idEnemigo = idEnemigo;
@@ -32,58 +54,114 @@ public class PaqueteAtacar extends Paquete implements Serializable, Cloneable {
 		mapEnemigo.put("probEvitarDanio", probEvitarDanoEnemgio);
 	}
 
+	/**
+	 * Devuelve la id del personaje
+	 * @return int
+	 */
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	/**
+	 * Establece la id del personaje
+	 * @param id id personaje
+	 */
+	public void setId(final int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Devuelve la id del enemigo
+	 * @return int
+	 */
 	public int getIdEnemigo() {
 		return idEnemigo;
 	}
 
-	public void setIdEnemigo(int idEnemigo) {
+	/**
+	 * Establece la id del enemigo
+	 * @param idEnemigo id enemigo
+	 */
+	public void setIdEnemigo(final int idEnemigo) {
 		this.idEnemigo = idEnemigo;
 	}
 
+	/**
+	 * Devuelve la salud del personaje
+	 * @return int
+	 */
 	public int getNuevaSaludPersonaje() {
 		return nuevaSaludPersonaje;
 	}
 
-	public void setNuevaSaludPersonaje(int nuevaSaludPersonaje) {
+	/**
+	 * Establece la salud del personaje
+	 * @param nuevaSaludPersonaje salud personaje
+	 */
+	public void setNuevaSaludPersonaje(final int nuevaSaludPersonaje) {
 		this.nuevaSaludPersonaje = nuevaSaludPersonaje;
 	}
 
+	/**
+	 * Devuelve la energia del personaje
+	 * @return int
+	 */
 	public int getNuevaEnergiaPersonaje() {
 		return nuevaEnergiaPersonaje;
 	}
 
-	public void setNuevaEnergiaPersonaje(int nuevaEnergiaPersonaje) {
+	/**
+	 * Establece la energia del personaje
+	 * @param nuevaEnergiaPersonaje energia personaje
+	 */
+	public void setNuevaEnergiaPersonaje(final int nuevaEnergiaPersonaje) {
 		this.nuevaEnergiaPersonaje = nuevaEnergiaPersonaje;
 	}
 
+	/**
+	 * Devuelve la salud del enemigo
+	 * @return int
+	 */
 	public int getNuevaSaludEnemigo() {
 		return nuevaSaludEnemigo;
 	}
 
-	public void setNuevaSaludEnemigo(int nuevaSaludEnemigo) {
+	/**
+	 * Establece la salud del enemigo
+	 * @param nuevaSaludEnemigo salud enemigo
+	 */
+	public void setNuevaSaludEnemigo(final int nuevaSaludEnemigo) {
 		this.nuevaSaludEnemigo = nuevaSaludEnemigo;
 	}
 
+	/**
+	 * Devuelve la energia del enemigo
+	 * @return int
+	 */
 	public int getNuevaEnergiaEnemigo() {
 		return nuevaEnergiaEnemigo;
 	}
 
-	public void setNuevaEnergiaEnemigo(int nuevaEnergiaEnemigo) {
+	/**
+	 * Establece la energia del enemigo
+	 * @param nuevaEnergiaEnemigo Energia enemigo
+	 */
+	public void setNuevaEnergiaEnemigo(final int nuevaEnergiaEnemigo) {
 		this.nuevaEnergiaEnemigo = nuevaEnergiaEnemigo;
 	}
 
+	/**
+	 * Devuelve el mapa del personaje
+	 * @return HashMap<String, Number>
+	 */
 	public HashMap<String, Number> getMapPersonaje() {
 		return mapPersonaje;
 	}
 
+	/**
+	 * Devuelve el mapa del enemigo
+	 * @return HashMap<String, Number>
+	 */
 	public HashMap<String, Number> getMapEnemigo() {
 		return mapEnemigo;
 	}
