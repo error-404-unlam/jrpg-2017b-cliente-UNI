4eeb6ba3919e8b9f59a9fcf08ff9b778d4e1a709
Miguel Angel Amengual <amengualma6@gmail.com>
2017-11-02 15:41:33 -0300

    Correccion de Incidencias MenuRegistro

--- src/main/java/frames/MenuRegistro.java
+++ src/main/java/frames/MenuRegistro.java
@@ -24,19 +24,80 @@ import javax.swing.JTextField;
 import cliente.Cliente;
 import mensajeria.Comando;
 
+/**
+ * Clase encargada del registro del usuario.
+ * @author Miguel
+ */
 public class MenuRegistro extends JFrame {
 
 	private JTextField txtUsuario;
 	private JPasswordField pwPassword;
 
+	//USUARIO
+	private static final int POS_X_USER = 199;
+	private static final int POS_Y_USER = 69;
+	private static final int ANCHO_USER = 118;
+	private static final int ALTO_USER = 20;
+	private static final int COLUMNS_USER = 10;
+	//PASSWORD
+	private static final int POS_X_PASSWORD = 199;
+	private static final int POS_Y_PASSWORD = 120;
+	private static final int ANCHO_PASSWORD = 118;
+	private static final int ALTO_PASSWORD = 20;
+	//BACKGROUND
+	private static final int POS_X_BACKGROUND = 0;
+	private static final int POS_Y_BACKGROUND = 0;
+	private static final int ANCHO_BACKGROUND = 444;
+	private static final int ALTO_BACKGROUND = 271;
+	//LABEL REGISTRARSE
+	private static final int POS_X_LABEL_REGISTRARSE = 186;
+	private static final int POS_Y_LABEL_REGISTRARSE = 182;
+	private static final int ANCHO_LABEL_REGISTRARSE = 82;
+	private static final int ALTO_LABEL_REGISTRARSE = 23;
+	private static final int TAM_FUENTE_LABEL_REGISTRARSE = 15;
+	//BOTON REGISTRARSE
+	private static final int POS_X_BOTON_REGISTRARSE = 143;
+	private static final int POS_Y_BOTON_REGISTRARSE = 182;
+	private static final int ANCHO_BOTON_REGISTRARSE = 153;
+	private static final int ALTO_BOTON_REGISTRARSE = 23;
+	//VENTANA
+	private static final int POS_X_VENTANA = 100;
+	private static final int POS_Y_VENTANA = 100;
+	private static final int ANCHO_VENTANA = 450;
+	private static final int ALTO_VENTANA = 300;
+	//LAYERED PANE
+	private static final int POS_X_LAYERED_PANE = 0;
+	private static final int POS_Y_LAYERED_PANE = 0;
+	private static final int ANCHO_LAYERED_PANE = 444;
+	private static final int ALTO_LAYERED_PANE = 271;
+	//LABEL USUARIO
+	private static final int POS_X_LABEL_USUARIO = 113;
+	private static final int POS_Y_LABEL_USUARIO = 70;
+	private static final int ANCHO_LABEL_USUARIO = 57;
+	private static final int ALTO_LABEL_USUARIO = 19;
+	private static final int TAM_FUENTE_LABEL_USUARIO = 15;
+	//LABEL PASSWORD
+	private static final int POS_X_LABEL_PASSWORD = 113;
+	private static final int POS_Y_LABEL_PASSWORD = 121;
+	private static final int ANCHO_LABEL_PASSWORD = 65;
+	private static final int ALTO_LABEL_PASSWORD = 17;
+	private static final int TAM_FUENTE_LABEL_PASSWORD = 15;
+	/**
+	 * Constructor Menu Registro
+	 * @param cliente Cliente actual
+	 */
 	public MenuRegistro(final Cliente cliente) {
 		// Se inicializa ícono y cursor
 		setIconImage(Toolkit.getDefaultToolkit().getImage("src/main/java/frames/IconoWome.png"));
-		setCursor(Toolkit.getDefaultToolkit().createCustomCursor(new ImageIcon(MenuJugar.class.getResource("/cursor.png")).getImage(), new Point(0, 0), "custom cursor"));
+		setCursor(
+				Toolkit.getDefaultToolkit().createCustomCursor(
+						new ImageIcon(
+								MenuJugar.class.getResource("/cursor.png")).
+						getImage(), new Point(0, 0), "custom cursor"));
 
 		addWindowListener(new WindowAdapter() {
 			@Override
-			public void windowClosing(WindowEvent e) {
+			public void windowClosing(final WindowEvent e) {
 				synchronized (cliente) {
 					cliente.setAccion(Comando.SALIR);
 					cliente.notify();
@@ -48,34 +109,66 @@ public class MenuRegistro extends JFrame {
 		setTitle("WOME - Registrarse");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
-		setBounds(100, 100, 450, 300);
+		setBounds(
+				POS_X_VENTANA,
+				POS_Y_VENTANA,
+				ANCHO_VENTANA,
+				ALTO_VENTANA);
 		getContentPane().setLayout(null);
 		setLocationRelativeTo(null);
 
 		JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.setBounds(0, 0, 444, 271);
+		layeredPane.setBounds(
+				POS_X_LAYERED_PANE,
+				POS_Y_LAYERED_PANE,
+				ANCHO_LAYERED_PANE,
+				ALTO_LAYERED_PANE);
 		getContentPane().add(layeredPane);
 
 		JLabel lblUsuario = new JLabel("Usuario");
-		lblUsuario.setBounds(113, 70, 57, 19);
+		lblUsuario.setBounds(
+				POS_X_LABEL_USUARIO,
+				POS_Y_LABEL_USUARIO,
+				ANCHO_LABEL_USUARIO,
+				ALTO_LABEL_USUARIO);
 		layeredPane.add(lblUsuario, new Integer(1));
 		lblUsuario.setForeground(Color.WHITE);
-		lblUsuario.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblUsuario.setFont(new Font(
+				"Tahoma",
+				Font.PLAIN,
+				TAM_FUENTE_LABEL_USUARIO));
 
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(113, 121, 65, 17);
+		lblPassword.setBounds(
+				POS_X_LABEL_PASSWORD,
+				POS_Y_LABEL_PASSWORD,
+				ANCHO_LABEL_PASSWORD,
+				ALTO_LABEL_PASSWORD);
 		layeredPane.add(lblPassword, new Integer(1));
 		lblPassword.setForeground(Color.WHITE);
-		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblPassword.setFont(new Font(
+				"Tahoma",
+				Font.PLAIN,
+				TAM_FUENTE_LABEL_PASSWORD));
 
 		JLabel lblRegistrarse = new JLabel("Registrarse");
-		lblRegistrarse.setBounds(186, 182, 82, 23);
+		lblRegistrarse.setBounds(
+				POS_X_LABEL_REGISTRARSE,
+				POS_Y_LABEL_REGISTRARSE,
+				ANCHO_LABEL_REGISTRARSE,
+				ALTO_LABEL_REGISTRARSE);
 		layeredPane.add(lblRegistrarse, new Integer(2));
 		lblRegistrarse.setForeground(Color.WHITE);
-		lblRegistrarse.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblRegistrarse.setFont(new Font(
+				"Tahoma", Font.PLAIN,
+				TAM_FUENTE_LABEL_REGISTRARSE));
 
 		JButton btnRegistrarse = new JButton("");
-		btnRegistrarse.setBounds(143, 182, 153, 23);
+		btnRegistrarse.setBounds(
+				POS_X_BOTON_REGISTRARSE,
+				POS_Y_BOTON_REGISTRARSE,
+				ANCHO_BOTON_REGISTRARSE,
+				ALTO_BOTON_REGISTRARSE);
 		layeredPane.add(btnRegistrarse, new Integer(1));
 		btnRegistrarse.setFocusable(false);
 		btnRegistrarse.setIcon(new ImageIcon(MenuRegistro.class.getResource("/frames/BotonMenu.png")));
@@ -83,55 +176,87 @@ public class MenuRegistro extends JFrame {
 		pwPassword = new JPasswordField();
 		pwPassword.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(final ActionEvent e) {
 				logIn(cliente);
 				dispose();
 			}
 		});
-		pwPassword.setBounds(199, 120, 118, 20);
+		pwPassword.setBounds(
+				POS_X_PASSWORD,
+				POS_Y_PASSWORD,
+				ANCHO_PASSWORD,
+				ALTO_PASSWORD);
 		layeredPane.add(pwPassword, new Integer(1));
 
 		txtUsuario = new JTextField();
 		txtUsuario.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(final ActionEvent e) {
 				logIn(cliente);
 				dispose();
 			}
 		});
-		txtUsuario.setBounds(199, 69, 118, 20);
+		txtUsuario.setBounds(
+				POS_X_USER,
+				POS_Y_USER,
+				ANCHO_USER,
+				ALTO_USER);
 		layeredPane.add(txtUsuario, new Integer(1));
-		txtUsuario.setColumns(10);
+		txtUsuario.setColumns(COLUMNS_USER);
 
 		JLabel labelBackground = new JLabel("");
-		labelBackground.setBounds(0, 0, 444, 271);
+		labelBackground.setBounds(
+				POS_X_BACKGROUND,
+				POS_Y_BACKGROUND,
+				ANCHO_BACKGROUND,
+				ALTO_BACKGROUND);
 		layeredPane.add(labelBackground, new Integer(0));
 		labelBackground.setIcon(new ImageIcon(MenuRegistro.class.getResource("/frames/menuBackground.jpg")));
 		btnRegistrarse.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(final ActionEvent e) {
 				logIn(cliente);
 				dispose();
 			}
 		});
 	}
 
+	/**
+	 * Devuelve el campo del usuario
+	 * @return JTextField
+	 */
 	public JTextField gettxtUsuario() {
 		return txtUsuario;
 	}
 
-	public void settxtUsuario(JTextField txtUsuario) {
-		this.txtUsuario = txtUsuario;
+	/**
+	 * Setea el usuario
+	 * @param usuario campo de texto del usuario
+	 */
+	public void settxtUsuario(final JTextField usuario) {
+		this.txtUsuario = usuario;
 	}
 
+	/**
+	 * Devuelve la contraseña
+	 * @return JPasswordField
+	 */
 	public JPasswordField getPasswordField() {
 		return pwPassword;
 	}
 
-	public void setPasswordField(JPasswordField pwPassword) {
-		this.pwPassword = pwPassword;
+	/**
+	 * Setea la contraseña del usuario
+	 * @param password campo de contraseña
+	 */
+	public void setPasswordField(final JPasswordField password) {
+		this.pwPassword = password;
 	}
 
+	/**
+	 * Setea el usuario y la contraseña
+	 * @param cliente cliente actual
+	 */
 	private void logIn(final Cliente cliente) {
 		synchronized (cliente) {
 			cliente.getPaqueteUsuario().setUsername(txtUsuario.getText());
